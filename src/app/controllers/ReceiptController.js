7d058d208ba2b9731a47180f0c1a905c51c80620
Guilherme Ramalho <guilherme-ramalho@outlook.com>
2020-02-23 11:09:07 -0300

    Generating Base64

--- src/app/controllers/ReceiptController.js
+++ src/app/controllers/ReceiptController.js
@@ -1,6 +1,24 @@
+import { createCanvas } from 'canvas';
+
 class ReceiptController {
   generate(request, response) {
-    return response.json({ teste: 'teste' });
+    const canvas = createCanvas(200, 200);
+    const ctx = canvas.getContext('2d');
+
+    // Write "Awesome!"
+    ctx.font = '30px Impact';
+    ctx.rotate(0.1);
+    ctx.fillText('Awesome!', 50, 100);
+
+    // Draw line under text
+    const text = ctx.measureText('Awesome!');
+    ctx.strokeStyle = 'rgba(0,0,0,0.5)';
+    ctx.beginPath();
+    ctx.lineTo(50, 102);
+    ctx.lineTo(50 + text.width, 102);
+    ctx.stroke();
+
+    return response.json({ base64: canvas.toDataURL() });
   }
 }
 
