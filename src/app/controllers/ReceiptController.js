8ff424521332f047986646a27b53d494380c50d4
Guilherme Ramalho <guilherme-ramalho@outlook.com>
2020-02-27 10:17:23 -0300

    Code refactoring

--- src/app/controllers/ReceiptController.js
+++ src/app/controllers/ReceiptController.js
@@ -1,68 +1,24 @@
-import { createCanvas, loadImage } from 'canvas';
+import Receipt from '../models/Receipt';
 
 class ReceiptController {
   async generate(request, response) {
     try {
-      // constants
       const bet = request.body;
-      const canvasWidth = 700;
-      const canvasHeight = bet.eventos.length * 40;
-      const logoWidth = 250;
-      const logoHeight = 70;
-      const canvas = createCanvas(canvasWidth, canvasHeight);
-      const context = canvas.getContext('2d');
-      const headerRowHeight = 30;
-      const canvasMiddlePoint = canvas.width / 2;
-      context.font = '16px Courier new';
+
+      const receipt = new Receipt(bet);
 
       // setting canvas background color
-      context.fillStyle = '#ffffe6';
-      context.fillRect(0, 0, canvas.width, canvas.height);
-
-      const header = {
-        Apostador: bet.nomeApostador,
-        Valor: bet.valorApostado,
-        Código: bet.codigo,
-        Prêmio: bet.valorPremio,
-        Data: bet.data,
-        Status: bet.status,
-      };
-
-      let itemsPerRow = 1;
-      let loopCount = 0;
-
-      // drawing receipt header items
-      Object.entries(header).map(item => {
-        const title = item[0];
-        const value = item[1];
-        const xPoint = itemsPerRow === 2 ? canvasMiddlePoint : 0;
-        const yPoint = 90 + Math.floor(loopCount) * headerRowHeight;
-
-        context.fillStyle =
-          Math.ceil(loopCount) % 2 === 0 ? '#fafa89' : '#dbdb23';
-        context.fillRect(xPoint, yPoint, canvasMiddlePoint, headerRowHeight);
-        context.fillStyle = '#000';
-        context.font = '20px Georgia';
-        context.fillText(`${title}: ${value}`, xPoint + 10, yPoint + 25);
-
-        itemsPerRow = itemsPerRow === 2 ? 1 : itemsPerRow + 1;
-        loopCount += 0.5;
-      });
+      receipt.setBackgroundRect();
 
       // drawing logo on the top of the page
-      await loadImage(
-        'http://bet.msports.online/assets/images/navbar-logo.png'
-      ).then(image => {
-        context.drawImage(
-          image,
-          (canvasWidth - logoWidth) / 2,
-          10,
-          logoWidth,
-          logoHeight
-        );
-      });
+      await receipt.drawLogo();
 
-      return response.json({ base64: canvas.toDataURL() });
+      // drawing canvas header
+      receipt.drawHeader();
+
+      console.log('teste');
+
+      return response.json({ base64: receipt.getBase64() });
     } catch (error) {
       console.log(error);
       return response.json({
---
+++ src/app/models/Receipt.js
@@ -0,0 +1,92 @@
+import Intl from 'intl';
+import ptBrIntl from 'intl/locale-data/jsonp/pt-BR';
+import { createCanvas, loadImage } from 'canvas';
+
+class Receipt {
+  constructor(bet) {
+    this.bet = bet;
+    this.canvas = createCanvas(700, this.bet.eventos.length * 40);
+    this.context = this.canvas.getContext('2d');
+    this.logoWidth = 250;
+    this.logoHeight = 70;
+    this.canvasMiddlePoint = this.canvas.width / 2;
+    this.headerRowHeight = 30;
+    this.context.font = '16px Courier new';
+
+    // binding functions to the class context
+    this.formatMoney = this.formatMoney.bind(this);
+    this.drawHeader = this.drawHeader.bind(this);
+    this.setBackgroundRect = this.setBackgroundRect.bind(this);
+    this.drawLogo = this.drawLogo.bind(this);
+  }
+
+  formatMoney(value) {
+    return new Intl.NumberFormat(ptBrIntl, {
+      style: 'currency',
+      currency: 'BRL',
+    }).format(value);
+  }
+
+  setBackgroundRect() {
+    this.context.fillStyle = '#ffffe6';
+    this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+
+  drawHeader() {
+    const header = {
+      Apostador: this.bet.nomeApostador,
+      Valor: this.formatMoney(this.bet.valorApostado),
+      Código: this.bet.codigo,
+      Prêmio: this.formatMoney(this.bet.valorPremio),
+      Data: this.bet.data,
+      Status: this.bet.status,
+    };
+
+    let itemsPerRow = 1;
+    let loopCount = 0;
+
+    Object.entries(header).map(item => {
+      const title = item[0];
+      const value = item[1];
+      const xPoint = itemsPerRow === 2 ? this.canvasMiddlePoint : 0;
+      const yPoint = 90 + Math.floor(loopCount) * this.headerRowHeight;
+
+      this.context.fillStyle =
+        Math.ceil(loopCount) % 2 === 0 ? '#fafa89' : '#dbdb23';
+      this.context.fillRect(
+        xPoint,
+        yPoint,
+        this.canvasMiddlePoint,
+        this.headerRowHeight
+      );
+      this.context.fillStyle = '#000';
+      this.context.font = '20px Georgia';
+      this.context.fillText(`${title}: ${value}`, xPoint + 10, yPoint + 25);
+
+      itemsPerRow = itemsPerRow === 2 ? 1 : itemsPerRow + 1;
+      loopCount += 0.5;
+    });
+  }
+
+  async drawLogo() {
+    await loadImage(
+      'http://bet.msports.online/assets/images/navbar-logo.png'
+    ).then(image => {
+      console.log((this.canvas.width - this.logoWidth) / 2);
+
+      this.context.drawImage(
+        image,
+        (this.canvas.width - this.logoWidth) / 2,
+        10,
+        this.logoWidth,
+        this.logoHeight
+      );
+    });
+  }
+
+  getBase64() {
+    return this.canvas.toDataURL();
+  }
+}
+
+export default Receipt;
